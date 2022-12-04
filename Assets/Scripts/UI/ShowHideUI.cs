6a5c9610dc613ad3a249b690c6427f67cae0e25c
CasimusCasius <grzegorz.kazimierczak.sg@gmail.com>
2022-12-04 21:04:23 +0100

    Init

---
+++ Assets/Scripts/UI/ShowHideUI.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Inventory.UI
+{
+    public class ShowHideUI : MonoBehaviour
+    {
+        []
+        void Update()
+        {
+
+        }
+    }
+}
