6d97232e417bebe38133025fb94b0433c5c59a91
CasimusCasius <grzegorz.kazimierczak.sg@gmail.com>
2022-12-05 00:07:32 +0100

    Inventory Mock UI

--- Assets/Scripts/UI/ShowHideUI.cs
+++ Assets/Scripts/UI/ShowHideUI.cs
@@ -7,10 +7,25 @@ namespace Inventory.UI
 {
     public class ShowHideUI : MonoBehaviour
     {
-        []
+        [SerializeField] KeyCode showHideKey = KeyCode.I;
+        [SerializeField] GameObject objectToHide = null;
+
+        private void Start()
+        {
+            objectToHide.SetActive(false);
+        }
         void Update()
         {
+           if(Input.GetKeyDown(showHideKey))
+            {
+                ShowHide();
+            }
 
+        }
+
+        private void ShowHide()
+        {
+            objectToHide.SetActive(!objectToHide.activeSelf);
         }
     }
 }
